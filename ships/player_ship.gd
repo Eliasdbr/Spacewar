744147d3a6b36cbad4604f22939c9b30d9772465
eliasdbr <eliasdbr@outlook.com>
2024-01-03 21:11:33 -0300

    Ship scaled down

--- ships/player_ship.gd
+++ ships/player_ship.gd
@@ -1,12 +1,14 @@
 extends Ship
 
 @export_range(1, 2) var player_number: int = 1
-
+@export_color_no_alpha var line_color = Color(1.0, 1.0, 1.0)
 @onready var tank_controls = $TankControls
+@onready var out_line_2d = $OutLine2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#super()
 	tank_controls.player = player_number
-
+	out_line_2d.default_color = line_color
+	#out_line_2d.default_color = line_color

8ffa13d010494455e26076f94949e1fb1a3136ef
eliasdbr <eliasdbr@outlook.com>
2024-01-05 22:27:44 -0300

    Player indicators

---
+++ ui/player_indicator.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var player_label = %PlayerLabel
+@onready var score_label = %ScoreLabel
+@onready var margin_container = %MarginContainer
+
+@export_range(1,2) var player_number := 1
+
+@export var score := 0:
+	set(value):
+		score = value
+		score_label.text = "Score: " + str(score)
+
+func _ready():
+	player_label.text = "Player " + str(player_number)
+	score_label.text = "Score: " + str(score)
+	if (player_number % 2 == 0):
+		margin_container.anchor_right = 0.0

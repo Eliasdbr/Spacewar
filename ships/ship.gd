1f58de4a0e304433bb2e83f8a435907e988e6982
eliasdbr <eliasdbr@outlook.com>
2024-01-05 21:11:15 -0300

    Viewport resized, star gravity adjusted, ships initial velocity

--- ships/ship.gd
+++ ships/ship.gd
@@ -5,6 +5,8 @@ extends Node2D
 ## For gravity
 @export var mass := 1.0
 
+@onready var movement = $Movement
+
 func _ready():
 	rotation = randf_range(0.0, TAU)
-	
+	movement.velocity = movement.max_speed * 0.2 * Vector2.RIGHT.rotated(rotation)
